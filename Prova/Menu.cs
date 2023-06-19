11df115ed7ab2c69daa8be2ef7da9bce5937b831
Perolla <perolladiasmoreira@gmail.com>
2023-06-19 16:44:04 -0400

    first commit

---
+++ Prova/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tela2 serv = new tela2();
+                serv.ShowDialog();
+        }
+    }
+}
